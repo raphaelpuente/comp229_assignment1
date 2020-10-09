ef445e836ad9fe6feaadb3632ae25940c9b25e6d
Raphael Puente <raphael.puente@gmail.com>
2020-10-08 21:29:53 -0400

    created routes

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
-  { title: 'Express',
-    mainHeading: 'this is a Heading' 
-  });
+  res.render('index', { title: 'Home'});
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About'});
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects'});
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services'});
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Us'});
 });
 
 module.exports = router;
